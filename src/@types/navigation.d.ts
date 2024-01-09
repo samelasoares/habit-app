b0af8115303d26b04a2a15f78e8cff58ac8eead9
Barbara Soares <barbarasamela90@gmail.com>
2024-01-09 19:50:59 -0300

    feat: The basic structure of the screens, the application routes were created, the navigation routes were defined and the habit recording interface was created. I enabled scrolling, the backButton component and the checkbox were created. The list of checkboxes for each day of the week, check/uncheck function and confirm button have been finalized.

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+       home: underfined;
+       new: underfined;
+       habit: {
+        date: string;
+      }
+    }
+  }
+}
