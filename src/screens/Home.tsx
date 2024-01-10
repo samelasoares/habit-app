353109753f0e671342f5b2e45e3a579b8e6532f4
Barbara Soares <barbarasamela90@gmail.com>
2024-01-10 18:32:05 -0300

    feat:I reused the backButton, formatted it and displayed the date of the week and day/month. I created the progressBar component and used the CheckBox component.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
 import { View, Text, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 import { Header } from "../components/Header";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateRangeDatesFromYearStart();
@@ -11,6 +13,9 @@ const minimumSummaryDatesSizes = 18 * 6;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -33,7 +38,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay 
+              key={date.toISOString()} 
+              onPress={() => navigate('habit', {date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
